97a91b13cb3fd42a40fd42a122dfe8ca20b2e5fa
LeeChan <lee79102@hotmail.com>
2020-11-14 13:34:22 +0800

    feat: project init

--- app.js
+++ app.js
@@ -2,24 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.send('列出全部 Todo')
-  next()
 })
 
-app.get('/new', (req, res, next) => {
+app.get('/new', (req, res) => {
   res.send('新增 Todo 頁面')
-  next()
 })
  
-app.get('/:id', (req, res, next) => {
+app.get('/:id', (req, res) => {
   res.send('顯示一筆 Todo')
-  next()
 })
 
-app.post('/', (req, res, next) => {
+app.post('/', (req, res) => {
   res.send('新增一筆  Todo')
-  next()
 })
 
 app.listen(port, () => {
